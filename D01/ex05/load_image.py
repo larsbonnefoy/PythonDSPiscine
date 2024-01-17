6863dea01185b4c164c719c605b13428d34f8cab
Lars Bonnefoy <lars.bonnefoy@hotmail.com>
2024-01-17 12:24:54 +0100

    changed error catch in ex03 -> ex05

--- D01/ex05/load_image.py
+++ D01/ex05/load_image.py
@@ -13,10 +13,14 @@ def ft_load(path: str) -> np.array:
         img = np.asarray(Image.open(path))
         print("The shape of the image is " + str(np.shape(img)))
         return img
+    except TypeError as te:
+        print(f"TypeError: {te}, unable to convert to np array")
     except FileNotFoundError as fnfe:
         print(f"FileNotFoundError: {fnfe}")
+    except PermissionError as pe:
+        print(f"PermissionError: {pe}")
     except AttributeError as ae:
         print(f"AttributeError: {ae}")
     except PIL.UnidentifiedImageError as ui:
         print(f"Unvalid file format: {ui}")
-    return []
+    raise IOError("Could not open file")
--- D01/ex05/tester.py
+++ D01/ex05/tester.py
@@ -1,10 +1,16 @@
 from load_image import ft_load
 from pimp_image import ft_invert, ft_red, ft_blue, ft_green, ft_grey
+import numpy as np
 
-img = ft_load("landscape.jpg")
-ft_invert(img)
-ft_red(img)
-ft_green(img)
-ft_blue(img)
-ft_grey(img)
-print(ft_invert.__doc__)
+try:
+    img = ft_load("landscape.jpg")
+    print(np.shape(img))
+    print(img)
+    ft_invert(img)
+    ft_red(img)
+    ft_green(img)
+    ft_blue(img)
+    ft_grey(img)
+    print(ft_invert.__doc__)
+except IOError as ioe:
+    print(f"IOError: {ioe}")
